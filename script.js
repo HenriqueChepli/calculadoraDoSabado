421b0af3f664fa6d873bf58501dc292fc056c599
HenriqueChepli <henriquechepli@gmail.com>
2023-11-14 16:57:00 -0300

    feito

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
-function clq1() {
-    alert("Você clicou no Botão 1");
-}
-
-let bt2 = document.getElementById("bt2");
-bt2.addEventListener("click", clq2);
-
-function clq2() {
-    alert("Você clicou no Botão 2");
-}
 
 let valor1 = document.querySelector("#valor1");
 let valor2 = document.querySelector("#valor2");
+let adicao = document.querySelector("#adicao");
+let subtracao = document.querySelector("#subtracao");
+let divisao = document.querySelector("#divisao");
+let multiplicacao = document.querySelector("#multiplicacao");
 let resultado = document.querySelector("#resultado");
 
-valor1.addEventListener("keyup", function() {
-    somar(Number(valor1.value), Number(valor2.value));
+adicao.addEventListener("click", function() {
+    mais(Number(valor1.value), Number(valor2.value));
 });
 
-valor2.addEventListener("keyup", function() {
-    somar(Number(valor1.value), Number(valor2.value));
+subtracao.addEventListener("click", function(){
+    menos(Number(valor1.value), Number(valor2.value));
 });
 
-function somar(a, b) {
+divisao.addEventListener("click", function(){
+    sobre(Number(valor1.value), Number(valor2.value));
+});
+
+multiplicacao.addEventListener("click", function(){
+    vezes(Number(valor1.value), Number(valor2.value));
+});
+
+function mais(a, b) {
     let soma = a + b;
     resultado.innerHTML = soma;
 }
+function menos(a, b) {
+    let soma = a - b;
+    resultado.innerHTML = soma;
+}
+function sobre(a, b) {
+    let soma = a / b;
+    resultado.innerHTML = soma;
+}
+function vezes(a, b) {
+    let soma = a * b;
+    resultado.innerHTML = soma;
+}
+
+let selectedLanguage = document.querySelector("#idioma");
+selectedLanguage.addEventListener("change", function(){
+    if(this.value == "br"){
+        titulo.textContent = "Calculadora"
+    }
+    else if(this.value == "es"){
+        titulo.textContent = "Calculadora"
+    }
+    else if(this.value == "en"){
+        titulo.textContent = "Calculator"
+    } 
+});
